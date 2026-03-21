4d97d9c9f03cf1858b20a6caea8a99773c0e67a4
AnonHarmony500 <github.ditch321@passmail.com>
2026-03-21 14:49:12 -0500

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,8 +1,8 @@
 exports.handler = async (event) => {
   const path = event.path.replace("/", "");
 
-  const BIN_ID = "YOUR_BIN_ID";
-  const API_KEY = "YOUR_API_KEY";
+  const BIN_ID = "69beeb75b7ec241ddc8d1815";
+  const API_KEY = "$2a$10$Fk35Luoxc3X.LjEWROkpI.hrsnL0KYwRNlBJbOACT9iMbPAv/t7M.";
 
   const res = await fetch(`https://api.jsonbin.io/v3/b/${BIN_ID}/latest`, {
     headers: {
