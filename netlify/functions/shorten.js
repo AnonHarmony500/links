b7b725bf5d28960009c0b0f9c282811b7503f1eb
AnonHarmony500 <github.ditch321@passmail.com>
2026-03-21 14:34:51 -0500

    Update shorten.js

--- netlify/functions/shorten.js
+++ netlify/functions/shorten.js
@@ -37,7 +37,7 @@ exports.handler = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      shortUrl: `https://yourdomain.com/${code}`
+      shortUrl: `https://nemesisharmony.com/${code}`
     })
   };
 };
